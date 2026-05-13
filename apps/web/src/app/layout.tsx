0ea0a5cb5e5847417bb434d0bfc29ab8c08775f8
lauragabryjanczyk <laura@ilex.net.pl>
2026-05-13 19:31:31 +0100

    Favicon set

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -17,9 +17,6 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "DETI MakerLab",
   description: "The central information system for projects, groups, and equipment requisitions at DETI.",
-  icons: {
-    icon: "/favicon.ico"
-  }
 };
 
 export default function RootLayout({
